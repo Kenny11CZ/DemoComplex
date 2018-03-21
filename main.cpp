8e3ae700ea46777825b5b2eaffb3a5f8ef505fd4
Dan Balarin <dan.balarin@gmail.com>
2018-03-21 12:27:50 +0100

    A house divided againts itself cannot stand

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@ int main() {
     Complex c1(10, 20);
     Complex c2(5, 5);
     Complex c3 = c1 + c2;
-//    cout << "C1" << endl;
-//    c1.print();
-//    cout << "C2" << endl;
-//    c2.print();
-//    cout << "C1+C2" << endl;
-//    c3.print();
+    cout << "C1" << endl;
+    c1.print();
+    cout << "C2" << endl;
+    c2.print();
+    cout << "C1+C2" << endl;
+    c3.print();
+    cout << endl;
 
     // Definitely lost
     cout << "Definitely lost" << endl;
@@ -20,11 +21,13 @@ int main() {
     c4 = new Complex(0, 0);
     c4->print();
     delete c4;
-
+    cout << endl;
+    
+    int *arr = new int[2];
     // Invalid read
     cout << "Invalid read" << endl;
-    int *arr = new int[2];
-    cout << arr[0] << arr[1] << arr[2] << endl;
+    cout << arr[0] << arr[1] << arr[2] << endl << endl;
+
     // Invalid write
     cout << "Invalid write" << endl;
     arr[2] = 1;
