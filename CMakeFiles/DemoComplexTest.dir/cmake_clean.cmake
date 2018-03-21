ae316d101ee4ebda7445c4dc209921f3a4767a2e
Dan Balarin <dan.balarin@gmail.com>
2018-03-21 01:38:17 +0100

    Lowered version of CMake dep

---
+++ CMakeFiles/DemoComplexTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DemoComplexTest.dir/tests.cpp.o"
+  "CMakeFiles/DemoComplexTest.dir/Complex.cpp.o"
+  "DemoComplexTest.pdb"
+  "DemoComplexTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DemoComplexTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
