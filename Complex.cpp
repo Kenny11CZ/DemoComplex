25fd14f02ebf891a29bac24bfe6df39882269d2b
Dan Balarin <dan.balarin@gmail.com>
2018-03-21 11:58:22 +0100

    Sumting

--- Complex.cpp
+++ Complex.cpp
@@ -8,9 +8,9 @@ Complex Complex::operator+(const Complex &c) const {
     return Complex(this->real+c.real, this->imaginary+c.imaginary);
 }
 
-void Complex::print() {
-    cout << "Real: " << real << " Imaginary: " << imaginary;
-}
+//void Complex::print() {
+//    cout << "Real: " << real << " Imaginary: " << imaginary << endl;
+//}
 
 ostream &operator<<(ostream &os, const Complex &complex) {
     os << "Real: " << complex.real << " Imaginary: " << complex.imaginary;
